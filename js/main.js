eeb6e88ae9a0872f6759934fd443bb4c72ca07c1
ISAI <isai.ismael1999@gmail.com>
2021-03-05 14:56:44 -0500

    version 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 (function () {
-    
+
+    window.addEventListener("resize", function () {
+        mostrarMenu();
+    })
+    window.addEventListener("load", function () {
+        mostrarMenu();
+    })
+    function mostrarMenu() {
+        let ancho = window.innerWidth;
+        let menu = document.querySelector(".menu");
+        let menu__ul = document.querySelector(".menu__ul");
+        let content = document.querySelector(".content")
+        let div__correo=document.querySelector(".div__correo")
+        let correo=document.querySelector(".correo")
+
+        if (ancho <= 980) {
+            content.appendChild(menu)
+            content.appendChild(correo)
+        } else {
+            menu__ul.appendChild(menu)
+            div__correo.appendChild(correo)
+        }
+    }
 
 }())
